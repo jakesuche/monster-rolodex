50fad40ba86a6939cf7df98a3301b8dc995688ed
uche jake <uchechidi9@gmail.com>
2020-05-23 03:35:32 +0100

    Updates

---
+++ precache-manifest.f6e633d295ccf8b97617229932ead9f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4723dc91c6888de47c5901a145461c3c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "232b46aa353d4f235616",
+    "url": "/monster-rolodex/static/css/main.bd91a77f.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232b46aa353d4f235616",
+    "url": "/monster-rolodex/static/js/main.86dbfef1.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
